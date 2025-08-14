c2d24245ad3f0e2eb7a251a2c7b7d1c2fe5ce44f
wncfht <2130212584@qq.com>
2025-08-14 09:09:25 +0800

    chore: add tf_events

--- src/infer/test_seqrec_without_itemid.py
+++ src/infer/test_seqrec_without_itemid.py
@@ -4,16 +4,16 @@ from src.config import parse_args
 from src.data import SeqRectWithoutItemIDDataset_1
 
 args = parse_args()
-dataset = SeqRectWithoutItemIDDataset_1(args, mode="train")
+dataset = SeqRectWithoutItemIDDataset_1(args, mode="test")
 print(dataset[0])
 
 from transformers import AutoProcessor, Qwen2_5_VLForConditionalGeneration
 
-from src.collator import MultiModalCollator
+from src.collator import UnifiedTestCollator
 
 ckpt_path = os.environ.get("CKPT_PATH")
-tokenizer = AutoProcessor.from_pretrained(ckpt_path)
-collator = MultiModalCollator(args, processor_or_tokenizer=tokenizer)
+processor = AutoProcessor.from_pretrained(ckpt_path)
+collator = UnifiedTestCollator(args, processor_or_tokenizer=processor)
 model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
     ckpt_path, trust_remote_code=True
 )
@@ -23,9 +23,10 @@ model.to("cuda")
 length = len(dataset)
 for i in range(length - 11, length):
     inputs = collator([dataset[i]])
+    print("Inputs:", "=" * 50)
     print(dataset[i].label_text)
-    inputs = {k: v.to("cuda") for k, v in inputs.items()}
+    inputs = {k: v.to("cuda") for k, v in inputs[0].items()}
     results = model.generate(**inputs)
 
     print("Outputs:", "=" * 50)
-    print(tokenizer.decode(results[0], skip_special_tokens=True))
+    print(processor.tokenizer.decode(results[0], skip_special_tokens=True))
