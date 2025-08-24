1ebbcd60e38b61e9d743fdc4e5a81162dd11abc7
wncfht <2130212584@qq.com>
2025-08-24 19:43:44 +0800

    feat: implement info_nce

---
+++ scripts/finetune/info_nce_seqrec_2_2B.sh
@@ -0,0 +1,30 @@
+export CUDA_VISIBLE_DEVICES=6
+
+DATASET=Instruments
+BASE_MODEL=Qwen/Qwen2-VL-2B-Instruct
+MODEL_TYPE=qwen2_vl
+DATA_PATH=./data
+RATIO_DATASET=1
+OUTPUT_DIR=./ckpt/$DATASET/Qwen2-VL-2B-Instruct-seqrec-info_nce-$RATIO_DATASET-qwen7B
+
+python -m src.finetune.info_nce_seqrec_train \
+    --seed 42 \
+    --base_model $BASE_MODEL \
+    --model_type $MODEL_TYPE \
+    --output_dir $OUTPUT_DIR \
+    --dataset $DATASET \
+    --data_path $DATA_PATH \
+    --per_device_batch_size 2 \
+    --epochs 4 \
+    --weight_decay 0.01 \
+    --save_and_eval_strategy epoch \
+    --bf16 \
+    --freeze visual \
+    --only_train_response \
+    --tasks seqrec \
+    --train_prompt_sample_num 1 \
+    --train_data_sample_num 0 \
+    --ratio_dataset $RATIO_DATASET \
+    --index_file .index_qwen7B.json \
+    --temperature 0.1 \
+    --info_nce_weight 1.0
