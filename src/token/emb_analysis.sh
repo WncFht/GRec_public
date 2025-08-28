dc80dbd5686d6bc0681c02ff071e6c21c0ffeb2a
wncfht <2130212584@qq.com>
2025-08-28 15:38:22 +0800

    feat: update tsne

--- src/token/emb_analysis.sh
+++ src/token/emb_analysis.sh
@@ -1,4 +1,4 @@
-python -m src.token.visualize_token_embeddings \
-    --embedding_path ./ckpt/Instruments/Qwen2-VL-7B-Instruct-seqrec-item2index-1-qwen7B/checkpoint-84892 \
+python -m src.token.tsne \
+    --model_path ./ckpt/Instruments/Qwen2-VL-7B-Instruct-seqrec-item2index-1-qwen7B/checkpoint-84892 \
     --interactive \
     --method tsne
