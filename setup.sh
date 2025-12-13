0a2da3a03d5eeb651d590ee7c49860618dc93c69
wncfht <2130212584@qq.com>
2025-12-13 17:05:43 +0800

    feat: 添加训练脚本和设置，支持PPO风格的比率剪切和自定义奖励函数

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+conda create -n grec python==3.10
 conda install gxx \
     av \
     pandas \
@@ -12,16 +13,12 @@ conda install gxx \
     binutils_linux-64 -y
 
 pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0
-pip install packaging
 
-pip install packaging psutil ninja einops \
-  --index-url http://pip.sankuai.com/simple/ \
-  --extra-index-url http://pypi.sankuai.com/simple/ \
-  --trusted-host pip.sankuai.com \
-  --trusted-host pypi.sankuai.com
+pip install packaging psutil ninja einops
 
 pip install flash-attn==2.7.4.post1 --no-build-isolation
 
+pip install -r requirements_backup.txt
 cd trl
 pip install .
 
